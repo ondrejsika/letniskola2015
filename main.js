b1e27b1fd1af53b4432ced2dc90d62995f968e14
Ondrej Sika <ondrej@ondrejsika.com>
2015-08-29 18:14:18 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+$('.onas').hover(function(){
+    $('body').css("background-image", "url(onas.jpg)");
+    $('.onas').css("background-color", "white");
+    $('.onas a').css("color", "#3B464A");
+}, function () {
+    $('body').css("background-image", "url(bcg.jpg)");
+    $('.onas').css("background", "none");
+    $('.onas a').css("color", "white");
+})
+$('.kontakt').hover(function(){
+    $('body').css("background-image", "url(kontakt.jpg)");
+    $('.kontakt').css("background-color", "white");
+    $('.kontakt a').css("color", "#3B464A");
+}, function () {
+    $('body').css("background-image", "url(bcg.jpg)");
+    $('.kontakt').css("background", "none");
+    $('.kontakt a').css("color", "white");
+})
+$('.reference').hover(function(){
+    $('body').css("background-image", "url(bg.jpg)");
+    $('.reference').css("background-color", "white");
+    $('.reference a').css("color", "#3B464A");
+}, function () {
+    $('body').css("background-image", "url(bcg.jpg)");
+    $('.reference').css("background", "none");
+    $('.reference a').css("color", "white");
+})
+$('.nabidka').hover(function(){
+    $('body').css("background-image", "url(nabidka.jpg)");
+    $('.nabidka').css("background-color", "white");
+    $('.nabidka a').css("color", "#3B464A");
+}, function () {
+    $('body').css("background-image", "url(bcg.jpg)");
+    $('.nabidka').css("background", "none");
+    $('.nabidka a').css("color", "white");
+})
